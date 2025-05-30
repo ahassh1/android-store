5900eaaae78afe9df02d69dc8956a1f3c39633de
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-05-30 10:57:00 +0600

    try to project setup and file initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-2xl bg-red-500'>
+        <h2> hello polash</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
