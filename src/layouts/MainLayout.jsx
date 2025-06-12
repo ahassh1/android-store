8806fa82b26a151b68e04a7690d918dfdb16768c
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-12 18:29:46 +0600

    footer section done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
     return (
         <>
            <Navbar/>
            <Outlet/>
-            <h2>Footer</h2>
+          <Footer/>
         </>
     );
 };
