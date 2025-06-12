d3c0bde1c2866a83cfb3064586c58ce919ef5f89
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-12 17:46:36 +0600

    navbar all part done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,23 +30,22 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+          <li>
+            <NavLink className={({isActive})=> (isActive ? 'text-indigo-700': '')}
+             to="/">Home</NavLink >
+          </li>
+          <li>
+            <NavLink className={({isActive})=> (isActive ? 'text-indigo-700': '')} to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink  className={({isActive})=> (isActive ? 'text-indigo-700': '')} to="/cart">
+            <MdShoppingCart size={20}/>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink  className={({isActive})=> (isActive ? 'text-indigo-700': '')} to="/favorites"><MdBookmarkAdd size={20}/>
+          </NavLink>
+          </li>
           </ul>
         </div>
         <Link to="/" className="font-bold text-xl">
@@ -68,9 +67,10 @@ const Navbar = () => {
             </NavLink>
           </li>
           <li>
-            <NavLink  className={({isActive})=> (isActive ? 'text-indigo-700': '')} to="/favorites">Favorites</NavLink>
+            <NavLink  className={({isActive})=> (isActive ? 'text-indigo-700': '')} to="/favorites"><MdBookmarkAdd size={20}/>
+          </NavLink>
           </li>
-          <li></li>
+        
         </ul>
       </div>
     </div>
