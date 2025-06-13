4dd560e9ac08d9bcaf10060c100da9eed4568ceb
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-13 09:25:30 +0600

    just 6 card shown in phone container section to used slice method

--- src/components/PhonesContainer.jsx
+++ src/components/PhonesContainer.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PhoneCard from "../pages/PhoneCard";
 
 const PhonesContainer = ({ phones }) => {
-  console.log(phones);
+  const [displayPhones, setDisplayPhones] = useState([]);
+  //   console.log(phones);
+  useEffect(() =>{
+    setDisplayPhones(phones.slice(0,6))
+  },[])
+
   return (
     <div className="py-12">
       <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-7 mb-8">
-        {phones.map((phone) => (
+        {displayPhones.map((phone) => (
           <PhoneCard key={phone.id} phone={phone} />
         ))}
       </div>
@@ -14,7 +19,7 @@ const PhonesContainer = ({ phones }) => {
       <button className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-black  shadow-gray-700 shadow-md">
         <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
         <span className="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
-        Show All
+          Show All
         </span>
       </button>
     </div>
