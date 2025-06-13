3d43ad1d1e7d3d9ef20883c204d41509c57e1acf
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-13 08:38:08 +0600

    view more button added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,15 +15,27 @@ const Hero = () => {
           Browse, Search, View, Buy
         </h1>
         <p className="text-gray-500">
-            Best place to browse, search, view details and purchase of top flagship <br />phones of the current time - FlagshipFaceOff See More...
+          Best place to browse, search, view details and purchase of top
+          flagship <br />
+          phones of the current time - FlagshipFaceOff See More...
         </p>
 
         <form className="flex flex-col md:flex-row justify-center items-center mb-4 md:px-24">
-          <input type="text" placeholder="Search phone by name" className="bg-white border border-gray-400 rounder shadow-md w-2/3 h-12 px-4 mb-3 focus:outline-none focus:shadow-outline md:mr-2 md:mb-0" />
-          <button>Search</button>
+          <input
+            type="text"
+            placeholder="Search phone by name"
+            className="bg-white border border-gray-400 rounder shadow-md w-2/3 h-12 px-4 mb-3 focus:outline-none focus:shadow-outline md:mr-2 md:mb-0"
+          />
+          <button
+            className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-black  shadow-gray-700 shadow-md"
+          >
+            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+            <span className="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
+              View More
+            </span>
+          </button>
         </form>
       </div>
-
     </div>
   );
 };
--- src/pages/PhoneCard.jsx
+++ src/pages/PhoneCard.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import { Link } from "react-router";
 
-const PhoneCard = ({phone}) => {
-    const {name, image , description} = phone || {}
+const PhoneCard = ({ phone }) => {
+  const { name, image, description } = phone || {};
   return (
     <div>
       <div className="card bg-base-100 shadow-sm">
         <figure>
-          <img
-            src={image}
-            alt="Shoes"
-          />
+          <img src={image} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
-          <p>
-            {description}
-          </p>
+          <p>{description}</p>
           <div className="card-actions justify-end">
-          
-          <Link to='/phone-details'>
-            <button className="btn btn-primary">View more</button>
-          </Link>
+            <Link to="/phone-details">
+              <button className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 shadow-gray-700 shadow-md">
+                <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                <span className="relative text-black transition duration-300 group-hover:text-white ease">
+                  View More
+                </span>
+              </button>
+            </Link>
+
           </div>
+                <button
+            className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-black  shadow-gray-700 shadow-md"
+          >
+            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+            <span className="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
+              View More
+            </span>
+          </button>
         </div>
       </div>
     </div>
