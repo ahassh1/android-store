118d3b7e6260a57932b496d35e08681d2f69d3ff
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-13 12:25:26 +0600

    search item worked , step 3 done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hero from '../components/Hero';
 import PhonesContainer from '../components/PhonesContainer';
 import { useLoaderData } from 'react-router';
@@ -6,15 +6,21 @@ import { useLoaderData } from 'react-router';
 const Home = () => {
     // data
     const data = useLoaderData()
+    const [phones, setPhones] =useState(data)
     // console.log(data);
     const handleSearch = (e, text) =>{
         e.preventDefault()
-        console.log(text);
+        const searchedPhones = data.filter(phone => 
+            phone.name.toLowerCase().include(text.toLowerCase()) ||
+            phone.brand.toLowerCase().include(text.toLowerCase())
+    
+    )
+        console.log(searchedPhones);
     }
     return (
         <div>
             <Hero handleSearch={handleSearch}/>
-            <PhonesContainer phones={data}/>
+            <PhonesContainer phones={phones}/>
         </div>
     );
 };
