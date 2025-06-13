790d095c290be3fc5986ec2c614a5a9bbfd95ed3
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-13 09:39:36 +0600

    show all button is responding previous clicked

--- src/components/PhonesContainer.jsx
+++ src/components/PhonesContainer.jsx
@@ -3,10 +3,15 @@ import PhoneCard from "../pages/PhoneCard";
 
 const PhonesContainer = ({ phones }) => {
   const [displayPhones, setDisplayPhones] = useState([]);
-  //   console.log(phones);
-  useEffect(() =>{
-    setDisplayPhones(phones.slice(0,6))
-  },[])
+  const [showAll, setShowAll] = useState(false)
+  useEffect(() => {
+    // setDisplayPhones(phones.slice(0, 6));
+    if(showAll){
+        setDisplayPhones(phones)
+    }else{
+        setDisplayPhones(phones.slice(0,6))
+    }
+  }, [phones, showAll]);
 
   return (
     <div className="py-12">
@@ -16,7 +21,7 @@ const PhonesContainer = ({ phones }) => {
         ))}
       </div>
 
-      <button className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-black  shadow-gray-700 shadow-md">
+      <button onClick={()=> setShowAll(prv => !prv)} className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-black  shadow-gray-700 shadow-md">
         <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
         <span className="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
           Show All
