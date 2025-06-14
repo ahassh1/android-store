01ecac65fadf8c9cf90d6232714e6d242c276fd4
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-14 19:21:40 +0600

    local host card items show in ui

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PhoneCard from './PhoneCard';
+import { getFavorites } from '../utils';
 
 const Favorites = () => {
+      const [displayPhones, setDisplayPhones] = useState([]);
+      useEffect(() =>{
+        const savedPhones = getFavorites()
+        setDisplayPhones(savedPhones)
+      },[]);
     return (
-        <div>
-            <h1>favorites</h1> 
-        </div>
+          <div className="py-12">
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-7 mb-8">
+        {displayPhones.map((phone) => (
+          <PhoneCard key={phone.id} phone={phone} />
+        ))}
+      </div>
+
+  
+    </div>
     );
 };
 
