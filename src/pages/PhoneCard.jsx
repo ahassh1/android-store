449db011a47acc9dc6e1081f52f9f8b6e926d8b5
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-14 13:36:17 +0600

    try to create bug fix and image show in other route

--- src/pages/PhoneCard.jsx
+++ src/pages/PhoneCard.jsx
@@ -13,7 +13,7 @@ const PhoneCard = ({ phone }) => {
           <h2 className="card-title">{name}</h2>
           <p>{description}</p>
           <div className="card-actions justify-end">
-            <Link to="/phone-details/${id}">
+            <Link to={`/phone-details/${id}`}>
               <button className="rounded-md px-3.5 py-[9px] m-1 overflow-hidden relative group cursor-pointer border-2 font-medium
                border-indigo-600 shadow-gray-700 shadow-md">
                 <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
